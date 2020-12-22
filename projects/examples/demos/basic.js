e569098ba41ce96eb7e38e19a2ad36656bb382bf
Burhan Yılmaz <wburhany@gmail.com>
2020-12-22 15:02:16 +0300

    chore: folder moved

---
+++ projects/examples/demos/basic.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Calendar, Views } from 'react-big-calendar'
+import events from '../events'
+
+const allViews = Object.keys(Views).map(k => Views[k])
+
+const Basic = ({ localizer }) => (
+  <Calendar
+    events={events}
+    views={allViews}
+    timeslots={2}
+    showMultiDayTimes
+    localizer={localizer}
+    // defaultView="week"
+  />
+)
+
+export default Basic
